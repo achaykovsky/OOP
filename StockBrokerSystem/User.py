4b6cb7e8a1556f7f65f0086c9db8e06957bebed8
Anna Chaykovsky <annachaykovsky@gmail.com>
2025-04-06 15:45:38 +0300

    created the User class

---
+++ StockBrokerSystem/User.py
@@ -0,0 +1,24 @@
+from StockBrokerSystem.OperationType import OperationType
+
+
+class User:
+    def __init__(self, user_id):
+        self.user_id = user_id
+        self.enabled = True
+        self.permissions = {
+            OperationType.BUY: True,
+            OperationType.SELL: True,
+            OperationType.SHORT: True,
+        }
+
+    def disable(self):
+        self.enabled = False
+
+    def enable(self):
+        self.enabled = True
+
+    def disable_operation(self, operation_type):
+        self.permissions[operation_type] = False
+
+    def enable_operation(self, operation_type):
+        self.permissions[operation_type] = True
